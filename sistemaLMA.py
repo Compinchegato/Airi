7d191d4b77539fd03f64a9c4e35c73e298b1a321
Compinchegato <63562934+Compinchegato@users.noreply.github.com>
2021-08-12 04:28:04 +0200

    Add files via upload

---
+++ sistemaLMA.py
@@ -0,0 +1,12 @@
+print ("AIRI EJECUTÓ AA-23")
+
+nombre= "nombre:""Airi"
+apellido= "apellido:""Natsuki"
+apodo= "apodo:""021"
+
+print(nombre)
+print(apellido)
+print(apodo)
+
+print("papa lo siento pero no puedo conmigo misma. Dile a mama que la amo ♥️")
+open ("index.html")
